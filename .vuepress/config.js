073552bcdae80922f4dae28034d3390c0cb03bd4
gomezlopezdiego <diego1278@hotmail.com>
2022-10-05 10:47:01 -0500

    first commit

---
+++ .vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    title: 'Vuepress',
+    description: 'Just playing around',
+
+    themeConfig: {
+        nav: [
+          //{ text: 'Contenido', link: '/contenido/' },
+          { text: 'External', link: 'https://google.com' }
+        ],
+        
+        sidebar: {
+          '/gestionEfectiva/':[
+            '',
+          ],
+          'curso2':[
+            '',
+          ]
+
+        }
+      }
+
+  }
